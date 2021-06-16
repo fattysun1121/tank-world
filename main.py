ca3a7bb37c39d4e1a4ea0bbd250f5b60932bba5a
孫世弘 <fatfat@sunshihongs-MacBook-Pro.local>
2021-06-16 16:44:43 +0800

    Implement movement and growth mechanic of tank

--- main.py
+++ main.py
@@ -6,24 +6,22 @@ from pygame.locals import *
 
 WINDOWWIDTH = 400
 WINDOWHEIGHT = 400
-PLAYERSPEED = 5
+PLAYERSPEED = 25
 PLAYER_SIZE = 25
 BULLETSIZE = 15
-tanks = pygame.sprite.Group()
+tanks = []
 bullets = pygame.sprite.Group()
 all_items = pygame.sprite.Group()
+body_group = pygame.sprite.Group()
 bullet_counter = 0
-ADD_BULLET = 50
-
-
-def terminate():
-    pygame.quit()
-    sys.exit()
+ADD_BULLET = 25
 
 
 class Tank(pygame.sprite.Sprite):
-    def __init__(self, dir='up', x=200, y=200):
+    def __init__(self, dir='down', x=200, y=200, crossing=False):
         super().__init__()
+        self.dir = dir
+        self.crossing = crossing
         self.surfImage = {
             'up': [
                 pygame.image.load(
@@ -56,21 +54,18 @@ class Tank(pygame.sprite.Sprite):
             ]
         }
         self.surf = pygame.transform.scale(
-            self.surfImage['up'][0], (PLAYER_SIZE, PLAYER_SIZE))
+            self.surfImage[self.dir][0], (PLAYER_SIZE, PLAYER_SIZE))
         self.rect = pygame.Rect(x, y, PLAYER_SIZE, PLAYER_SIZE)
-        self.moving = False
-        self.has_fire = False
+
         self.move_count = 0
-        self.fire_count = 0
-        self.dir = dir
 
     def move(self, direction):
-        if self.moving:
-            self.move_count += 1
-            self.surf = pygame.transform.scale(
-                self.surfImage[direction][self.move_count], (PLAYER_SIZE, PLAYER_SIZE))
-            if self.move_count == 4:
-                self.move_count = -1
+
+        self.move_count += 1
+        self.surf = pygame.transform.scale(
+            self.surfImage[direction][self.move_count], (PLAYER_SIZE, PLAYER_SIZE))
+        if self.move_count == 4:
+            self.move_count = -1
         if self.dir != direction:
             self.dir = direction
             self.rect == self.surf.get_rect(
@@ -96,21 +91,56 @@ def gen_bullet():
     all_items.add(bullet)
 
 
+def gen_tank():
+    previous = tanks[-1]
+    if previous.dir == 'up':
+        body = Tank(previous.dir, previous.rect.left, previous.rect.bottom)
+    elif previous.dir == 'down':
+        body = Tank(previous.dir, previous.rect.left,
+                    previous.rect.top - PLAYER_SIZE)
+    elif previous.dir == 'left':
+        body = Tank(previous.dir, previous.rect.right, previous.rect.top)
+    else:
+        body = Tank(previous.dir, previous.rect.left -
+                    PLAYER_SIZE, previous.rect.top)
+    tanks.append(body)
+    body_group.add(body)
+    all_items.add(body)
+
+
+def move_parts():
+
+    for i in range(len(tanks) - 1, 0, -1):
+        tanks[i].move(tanks[i - 1].dir)
+        tanks[i].rect.topleft = (
+            tanks[i - 1].rect.left, tanks[i - 1].rect.top)
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
 pygame.init()
 gameClock = pygame.time.Clock()
 screen = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
 pygame.display.set_caption('World of Tank')
-
 pygame.mixer.music.load('background.mp3')
 pygame.mixer.music.play(-1)
 eatSound = pygame.mixer.Sound('eat.mp3')
 
+
 head = Tank()
-tanks.add(head)
+
+tanks.append(head)
+
+
 all_items.add(head)
 moveLeft = moveRight = moveUp = moveDown = False
+running = True
 
 while True:
+
     bullet_counter += 1
     if bullet_counter == ADD_BULLET:
         gen_bullet()
@@ -150,53 +180,59 @@ while True:
                 terminate()
 
     screen.fill((255, 255, 255))
+    move_parts()
+    if moveLeft:
+        head.moving = True
+        head.move('left')
+        head.rect.move_ip(-1 * PLAYERSPEED, 0)
+    elif moveRight:
+        head.moving = True
+        head.move('right')
+        head.rect.move_ip(PLAYERSPEED, 0)
+    elif moveUp:
+        head.moving = True
+        head.move('up')
+        head.rect.move_ip(0, -1 * PLAYERSPEED)
+    elif moveDown:
+        head.moving = True
+        head.move('down')
+        head.rect.move_ip(0, PLAYERSPEED)
+
     for tank in tanks:
-        if moveLeft:
-            tank.moving = True
-            tank.move('left')
-            tank.rect.move_ip(-1 * PLAYERSPEED, 0)
-
-        elif moveRight:
-            tank.moving = True
-            tank.move('right')
-            tank.rect.move_ip(PLAYERSPEED, 0)
-
-        elif moveUp:
-            tank.moving = True
-            tank.move('up')
-            tank.rect.move_ip(0, -1 * PLAYERSPEED)
-
-        elif moveDown:
-            tank.moving = True
-            tank.move('down')
-
-            tank.rect.move_ip(0, PLAYERSPEED)
-
         # Connect borders.
         in_window = (tank.rect.left > 0 and tank.rect.right < WINDOWWIDTH and
                      tank.rect.top > 0 and tank.rect.bottom < WINDOWHEIGHT)
-        if tank.rect.left + PLAYER_SIZE <= 0 and not crossing:
+
+        # Record if the tank is crossing border
+        # Reason: e.g. when the tank moves out from the left,
+        # it'll appear at the right side and its x-coordinate
+        # will be greater than WINDOWWIDTH. Hence causing the tank
+        # to be transported back to the left side.
+        if tank.rect.left + PLAYER_SIZE <= 0 and not tank.crossing:
             tank.rect.left = WINDOWWIDTH + tank.rect.width
-            crossing = True
-        elif tank.rect.left >= WINDOWWIDTH and not crossing:
+            tank.crossing = True
+        elif tank.rect.left >= WINDOWWIDTH and not tank.crossing:
             tank.rect.left = 0 - tank.rect.width
-            crossing = True
-        elif tank.rect.top + PLAYER_SIZE <= 0 and not crossing:
+            tank.crossing = True
+        elif tank.rect.top + PLAYER_SIZE <= 0 and not tank.crossing:
             tank.rect.top = WINDOWHEIGHT
-            crossing = True
+            tank.crossing = True
         elif tank.rect.top >= WINDOWHEIGHT:
             tank.rect.bottom = 0
-            crossing = True
+            tank.crossing = True
         if in_window:
-            crossing = False
+            tank.crossing = False
 
+    if pygame.sprite.spritecollideany(head, body_group):
+        break
     for bullet in bullets:
         if head.rect.colliderect(bullet.rect):
             bullet.kill()
             eatSound.play()
+            gen_tank()
     for item in all_items:
         screen.blit(item.surf, item.rect)
 
     pygame.display.update()
 
-    gameClock.tick(25)
+    gameClock.tick(5)
